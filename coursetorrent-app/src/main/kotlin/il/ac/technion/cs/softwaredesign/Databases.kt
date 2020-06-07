48e3ed161a5981f1a143b156b62f38dc25d7a76a
nadavhalahmi <nadavhalahmi@campus.technion.ac.il>
2020-06-07 16:49:30 +0300

    restored databases.kt

--- coursetorrent-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/Databases.kt
+++ coursetorrent-app/src/main/kotlin/il/ac/technion/cs/softwaredesign/Databases.kt
@@ -26,24 +26,21 @@ class Databases @Inject constructor(private val db_factory: SecureStorageFactory
     /**
      * saves torrent to database as mentioned above
      */
-    fun addTorrent(hash: String, value: ByteArray, dict: TorrentDict): CompletableFuture<Unit>{
-        torrentsDB.thenCompose { db ->
-            storageManager.setExists(db, hash)
-            for (key in dict.keys) {
-                if (key == "announce" || key == "announce-list") {
-                    val range = dict.getRange(key)
-                    storageManager.setValue(
-                        db,
-                        hash,
-                        key,
-                        value.copyOfRange(dict[key]!!.startIndex(), dict[key]!!.endIndex())
-                    )
-                    db.write((hash + key).toByteArray(), value.copyOfRange(range.startIndex(), range.endIndex()))
-                }
+    fun addTorrent(hash: String, value: ByteArray, dict: TorrentDict){
+        val db = torrentsDB.get()
+        storageManager.setExists(db, hash)
+        for (key in dict.keys) {
+            if (key == "announce" || key == "announce-list") {
+                val range = dict.getRange(key)
+                storageManager.setValue(
+                    db,
+                    hash,
+                    key,
+                    value.copyOfRange(dict[key]!!.startIndex(), dict[key]!!.endIndex())
+                )
+                db.write((hash + key).toByteArray(), value.copyOfRange(range.startIndex(), range.endIndex()))
             }
-            return@thenCompose
         }
-        return CompletableFuture<Unit>()
     }
 
     fun torrentExists(hash: String): Boolean {
