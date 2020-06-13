96cfa8868bb7e50910846752f127a4644f3454d6
nadavhalahmi <nadavhalahmi@campus.technion.ac.il>
2020-06-13 16:21:11 +0300

    updated connects to remote peer test. now fails

--- coursetorrent-app/src/test/kotlin/il/ac/technion/cs/softwaredesign/MyTest.kt
+++ coursetorrent-app/src/test/kotlin/il/ac/technion/cs/softwaredesign/MyTest.kt
@@ -323,16 +323,33 @@ class MyTest {
     fun `connects to remote peer`() {
         val infohash = torrent.load(lame).get()
 
-        thread(start = true) {
-            val peerSever = ServerSocket(6888)
-            val sock = peerSever.accept()
-            val output = sock.inputStream.readNBytes(68)
-            sock.outputStream.write(output)
-        }
+        startPeerServer(infohash)
+
         assertDoesNotThrow {
             torrent.connect(infohash, KnownPeer("127.0.0.1", 6888, null)).get() //sends handshake to peer and waits for response
         }
 
+        val knownPeers = torrent.knownPeers(infohash).get()
+        assert(knownPeers.isNotEmpty())
+        assertThat(
+                knownPeers,
+                anyElement(has(KnownPeer::peerId, equalTo(infohash.reversed()))))
+
+
+    }
+
+    private fun startPeerServer(infohash : String) {
+        thread(start = true) {
+            val peerSever = ServerSocket(6888)
+            val sock = peerSever.accept()
+            val output = sock.inputStream.readNBytes(68)
+            sock.outputStream.write(
+                    WireProtocolEncoder.handshake(
+                            hexStringToByteArray(infohash),
+                            hexStringToByteArray(infohash.reversed())
+                    )
+            )
+        }
     }
 
     private fun initiateRemotePeer(infohash: String): Socket {
